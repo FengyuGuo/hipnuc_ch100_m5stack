1f2bb9534a53c6c1e97ed9ba4a4fd8b14306c5e6
Guo, Fengyu <fyguo@telenav.cn>
2025-04-21 19:56:34 +0800

    add multi task

--- imu_decode/imu_decode.ino
+++ imu_decode/imu_decode.ino
@@ -5,6 +5,10 @@
 */
 
 #include <M5Stack.h>
+#include "esp_timer.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
 
 #define FRAME_HEAD1 0x5A
 #define FRAME_HEAD2 0xA5
@@ -50,14 +54,29 @@ static void crc16_update(uint16_t *currectCrc, const
     *currectCrc = crc;
 }
 
+QueueHandle_t imu_q;
+void process_task(void *param);
+
 void setup() {
     M5.begin();
     M5.Power.begin();
     IMURaw.begin(115200);
     Serial.println("hello");
-//    termInit();
     M5.Lcd.setTextFont(4);
     M5.Lcd.println("IMU Raw Example");
+    imu_q = xQueueCreate(10, sizeof(HipNucIMU));
+    if (!imu_q) {
+      Serial.println("Queue create failed!");
+    }
+    xTaskCreatePinnedToCore(
+      process_task,         // 任务函数
+      "print_imu",    // 任务名
+      1024,              // 栈大小（word 单位 = 1024*4 = 4KB）
+      NULL,              // 传参
+      1,                 // 优先级
+      NULL,              // 不需要句柄
+      0                  // 运行在 Core 0
+     );
 }
 
 enum ParseState {
@@ -146,7 +165,7 @@ void loop() {
         if (buf_index >= IMU_DATA_LENGTH) 
         {
           //check the checksum!
-
+          
           checksum = 0;
 //          Serial.write("got imu frame\n");
           crc16_update(&checksum, buffer, 4);
@@ -155,6 +174,7 @@ void loop() {
           {
 //            Serial.print("crc ok\n");
             memcpy(&imu_data, &buffer[6], sizeof(HipNucIMU));
+            xQueueSend(imu_q, &imu_data, 0);
 //          Serial.printf("size of imu data: %d\n", sizeof(HipNucIMU));
 //            Serial.printf("id, tag, timestamp, gyr_x:0x%02X, %u, %u, %f\n", imu_data.tag, imu_data.id, imu_data.timestamp, imu_data.gyr[0]);
             
@@ -166,16 +186,23 @@ void loop() {
             wrong_frames++;
           }
 
+
           if((right_frames + wrong_frames) % 100 == 0)
           {
+            int64_t start = esp_timer_get_time();
             Serial.printf("%u right frames got. %u wrong frames got\n", right_frames, wrong_frames);
-            M5.Lcd.fillScreen(0);
-            M5.Lcd.setCursor(0, 0);
-            sprintf(msg_str, "id: 0x%02X\ttag: %u\ntimestamp: %u\ngyr: %f, %f, %f\nacc: %f, %f, %f\nmag: %f, %f, %f\n", imu_data.tag, imu_data.id, imu_data.timestamp, 
-              imu_data.gyr[0], imu_data.gyr[1], imu_data.gyr[2],
-              imu_data.acc[0], imu_data.acc[1], imu_data.acc[2],
-              imu_data.mag[0], imu_data.mag[1], imu_data.mag[2]);
-            M5.Lcd.print(msg_str);
+//            M5.Lcd.fillScreen(0);
+//            M5.Lcd.setCursor(0, 0);
+//            sprintf(msg_str, "id: 0x%02X\ttag: %u\ntimestamp: %u\ngyr: %f, %f, %f\nacc: %f, %f, %f\nmag: %f, %f, %f\n", imu_data.tag, imu_data.id, imu_data.timestamp, 
+//              imu_data.gyr[0], imu_data.gyr[1], imu_data.gyr[2],
+//              imu_data.acc[0], imu_data.acc[1], imu_data.acc[2],
+//              imu_data.mag[0], imu_data.mag[1], imu_data.mag[2]);
+//            M5.Lcd.print(msg_str);
+            int64_t end = esp_timer_get_time();
+            
+            printf("viz cost: %lld us\n", end - start);
+//            int core = xPortGetCoreID();
+//            printf("Running on core %d\n", core);
           }
           
           state = WAIT_FOR_HEADER1;
@@ -194,5 +221,23 @@ void loop() {
     }
 
     // termPutchar(ch);
+  }
+}
+
+void process_task(void* param)
+{
+  HipNucIMU imu_data;
+  static int imu_cnt = 0;
+  while (1) {
+    if(pdTRUE == xQueueReceive(imu_q, &imu_data, 0))
+    {
+      imu_cnt++;
+      if(imu_cnt % 100 == 0)
+      {
+        printf("got imu %d, timestamp: %u\n", imu_cnt, imu_data.timestamp);
+      }
+      
+    }
+    vTaskDelay(1);
   }
 }
